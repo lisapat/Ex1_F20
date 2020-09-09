9b58aa4536ac6930ee43737382e5fe06be0d672e
luckyruby1 <lisapat@udel.edu>
2020-09-09 12:16:14 -0400

    updated to last, first name

---
+++ index.js
@@ -0,0 +1,16 @@
+$(function(){
+    $('#go').on("click", function(event) {
+        event.preventDefault();
+    
+        $('apples').text('#lname'.val() + ' ' + '#fname'.val());
+        $("apples").removeClass("d-none");
+        return false;
+      });
+
+      return false;
+
+    });
+// if both first and last name are filled then submit 
+// make the jumbotron last,first below the text fields
+// need to access button id (if clicked) then submit 
+// into the vars listed here and display as jumbotext
