5bb3ad5a85cb4ad9846216b93829ca3f684f16bf
luckyruby1 <lisapat@udel.edu>
2020-09-09 13:19:40 -0400

    Fixed JS file's order

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 $(function(){
+
     $("#go").on("click", function(event) {
         event.preventDefault();
     
-        $("#apples").text($("#lname").val() + " " + $("#fname").val());
         $("#apples").removeClass("d-none");
+        $("#apples").text($("#lname").val() + " " + $("#fname").val());
+      
         return false;
     })
 
     });
-// if both first and last name are filled then submit 
-// make the jumbotron last,first below the text fields
-// need to access button id (if clicked) then submit 
-// into the vars listed here and display as jumbotext
